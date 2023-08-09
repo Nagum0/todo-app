a16dc015dbb7b4798ffb565646c9ccb7891eb41a
Nagum0 <xpteeth@gmail.com>
2023-08-09 15:46:11 +0200

    Finished password strength check implementation.

--- static/js/sign_up.js
+++ static/js/sign_up.js
@@ -1,3 +1,23 @@
+// Checks if input has an uppercase letter.
+function hasUpperCase(input_text) {
+    return /[A-Z]/.test(input_text)
+}
+
+// Checks if input has a lowercase letter.
+function hasLowerCase(input_text) {
+    return /[a-z]/.test(input_text)
+}
+
+// Checks if input has a digit.
+function hasDigit(input_text) {
+    return /[\d]/.test(input_text)
+}
+
+// Checks if input has a symbol.
+function hasSymbol(input_text) {
+    return /[!@#$%^&*()_+{}\[\]:;<>,.?~\\\-]/.test(input_text)
+}
+
 const username_sign_up_input = document.getElementById("usernameSignUpInput")
 const email_input = document.getElementById("emailInput")
 const password_sign_up_input = document.getElementById("passwordSignUpInput")
@@ -13,34 +33,46 @@ login_btn.addEventListener("click", () => {
 
 /* Sign up btn */
 sign_up_btn.addEventListener("click", () => {
+    // Checks if password and confirm passwords match.
     if (password_sign_up_input.value != confirm_password_sign_up_input.value) {
-        error_msg_p.innerText = "Password was not confirmed."
+        error_msg_p.innerText = "Password and confirm passwords don't match."
     }
     else {
-        let sign_up_data = {
-            "username": username_sign_up_input.value,
-            "email": email_input.value,
-            "password": password_sign_up_input.value
-        }
+        // Checks password strength
+        if (hasUpperCase(password_sign_up_input.value) 
+        && hasLowerCase(password_sign_up_input.value) 
+        && hasDigit(password_sign_up_input.value) 
+        && hasSymbol(password_sign_up_input.value)) {
 
-        fetch("/signup", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(sign_up_data)
-        })
-        .then((res) => {
-            if (res.redirected) {
-                window.location.href = res.url
+            let sign_up_data = {
+                "username": username_sign_up_input.value,
+                "email": email_input.value,
+                "password": password_sign_up_input.value
             }
-            else {
-                return res.json()
-            }
-        })
-        .then(data => {
-            error_msg_p.innerText = data["msg"]
-        })
-        .catch(err => console.log("Error"))
+
+            // Post request
+            fetch("/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(sign_up_data)
+            })
+            .then((res) => {
+                if (res.redirected) {
+                    window.location.href = res.url
+                }
+                else {
+                    return res.json()
+                }
+            })
+            .then(data => {
+                error_msg_p.innerText = data["msg"]
+            })
+            .catch(err => console.log("Error"))
+        }
+        else {
+            error_msg_p.innerText = "Password is not strong enough.\nYou must have an uppercase letter, a lowercase letter\na number and a symbol!"
+        }
     }
 })
